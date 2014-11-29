25f35e099329d428a18accb4db3d879adcb3a3b8
Evan Ische <SixTilesSquared@gmail.com>
2014-11-29 17:12:12 -0600

    Added HDSwitch, a replacement for UISwitch(UIControl subclass, my first one) that isn't round.

---
+++ HexitSpriteKit/HDSwitch.h
@@ -0,0 +1,13 @@
+//
+//  HDSwitch.h
+//  HexitSpriteKit
+//
+//  Created by Evan Ische on 11/29/14.
+//  Copyright (c) 2014 Evan William Ische. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HDSwitch : UIControl
+
+@end
---
+++ HexitSpriteKit/HDSwitch.m
@@ -0,0 +1,13 @@
+//
+//  HDSwitch.m
+//  HexitSpriteKit
+//
+//  Created by Evan Ische on 11/29/14.
+//  Copyright (c) 2014 Evan William Ische. All rights reserved.
+//
+
+#import "HDSwitch.h"
+
+@implementation HDSwitch
+
+@end
